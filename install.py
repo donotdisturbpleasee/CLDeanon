a4c80d53d154fcd2fb65cfa7829cf27118dfa498
donotdisturbpleasee <78399949+donotdisturbpleasee@users.noreply.github.com>
2021-02-22 16:59:52 +1200

    Add files via upload

---
+++ install.py
@@ -0,0 +1,10 @@
+import os
+
+print("\033[33mОбновление python3-pip...\033[0m")
+os.system("pip3 install --upgrade pip")
+print("")
+bib = ["html2text", "requests", "bs4",  "urllib3"]
+for i in range(len(bib)):
+    print("\033[33mУстановка "+bib[i]+"...\033[0m")
+    os.system("pip3 install "+bib[i])
+print('\033[32mУстановка завершена!')
