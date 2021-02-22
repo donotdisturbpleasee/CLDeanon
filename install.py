f07070ccd6ca03e5f2fb304e62f5452c072a4787
donotdisturbpleasee <78399949+donotdisturbpleasee@users.noreply.github.com>
2021-02-22 17:04:35 +1200

    Update install.py

--- install.py
+++ install.py
@@ -7,4 +7,4 @@ bib = ["html2text", "requests", "bs4",  "urllib3"]
 for i in range(len(bib)):
     print("\033[33mУстановка "+bib[i]+"...\033[0m")
     os.system("pip3 install "+bib[i])
-print('\033[32mУстановка завершена!')
+print('\033[32mУстановка завершена!\033[0m')
